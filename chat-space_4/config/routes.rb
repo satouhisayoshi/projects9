61f14d8ee7fcf459c9be9a541017040c8772bc77
hisayoshi sato <45346449+satohisayoshi@users.noreply.github.com>
2019-04-02 20:33:44 +0900

    Routes

--- chat-space_4/config/routes.rb
+++ chat-space_4/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'messages' => 'messages#index'
-  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
 end
 
